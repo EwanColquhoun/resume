f4a9e032424081bf1429449c83e220ce1e459e0e
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-09-20 08:53:53 +0000

    add google map

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function ininMap() {
+    var map = new google.maps.map(document.getElementById('map'), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+}
